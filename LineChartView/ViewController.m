68140e9336d8f43b528063c3e825210c6fe11ffe
Superman <703353105@qq.com>
2018-08-10 10:28:26 +0800

    修改代码

--- LineChartView/ViewController.m
+++ LineChartView/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "ChartView.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) ChartView *incomeChartLineView;
 
 @end
 
@@ -17,8 +20,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self createIncomeChartLineView];
+
+}
+-(void)createIncomeChartLineView{
+    
+    NSArray *tempDataArrOfY = @[@"0.2",@"0.4",@"0.6",@"0.2",@"0.7",@"0.5",@"0.8"];
+    
+    //循环拿到收益最大值
+    float tempMax = [tempDataArrOfY[0] floatValue];
+    for (int i = 1; i < tempDataArrOfY.count; ++i) {
+        if ([tempDataArrOfY[i] floatValue] > tempMax) {
+            tempMax = [tempDataArrOfY[i] floatValue];
+        }
+    }
+    
+    _incomeChartLineView = [[ChartView alloc]initWithFrame:CGRectMake(0, 40, self.view.bounds.size.width, 200)];
+    _incomeChartLineView.backgroundColor = [UIColor clearColor];
+    
+    //    _incomeChartLineView.leftDataArr =  @[@"0.2",@"0.4",@"0.6",@"0.2",@"0.7",@"0.5",@"0.8"];
+    //    _incomeChartLineView.leftDataArr =  @[@"0.4",@"0.4",@"0.4",@"0.4",@"0.4",@"0.4"];
+    
+    _incomeChartLineView.titleOfYStr = @"七日收益(元)";
+    _incomeChartLineView.titleOfXStr = @"(日)";
+    _incomeChartLineView.leftDataArr = tempDataArrOfY;
+    _incomeChartLineView.dataArrOfY = @[@"12",@"10",@"8",@"6",@"4",@"2",@"0"];//拿到Y轴坐标
+    _incomeChartLineView.dataArrOfX = @[@"1",@"2",@"3",@"4",@"5",@"6",@"7"];//拿到X轴坐标
+    [self.view addSubview:_incomeChartLineView];
     
 }
+
 
 
 - (void)didReceiveMemoryWarning {
